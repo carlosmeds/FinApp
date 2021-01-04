b55876df47dcdd09860c19204e5cfb4d609bf957
carlosdaniel99 <cdmnkg@gmail.com>
2021-01-04 11:34:00 -0300

    feature: exit app

--- app/src/main/java/br/ufpr/tads/finapp/DashboardActivity.java
+++ app/src/main/java/br/ufpr/tads/finapp/DashboardActivity.java
@@ -3,6 +3,7 @@ package br.ufpr.tads.finapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    public void quitApp(View view) {
+        finishAndRemoveTask();
+        System.exit(0);
     }
 }
